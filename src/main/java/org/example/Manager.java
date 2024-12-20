aab72efe3679d790aae374e2ab9fdfe4eca469f8
PavelUkhov <164402643+PaulUkhov@users.noreply.github.com>
2024-12-20 15:10:39 +0300

    20.12.2024 15:19

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -6,15 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Manager extends Employee {
-    String name;
-    Integer salary;
     @Getter
     List<Employee> employees = new ArrayList<>();
 
 
     public Manager(Integer year, Integer month, Integer day, Integer salary, String name) {
         super(year, month, day, salary, name);
-        this.name = name;
+
 
     }
 
